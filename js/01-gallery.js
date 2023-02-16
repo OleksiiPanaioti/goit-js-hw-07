fb1dffa60990c711ba2a73edc33878b9f9d3ab6b
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-16 10:11:23 +0200

    task 1-1(create pics grid)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const galleryContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createImagesCardsMarkup(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
+
+function createImagesCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return ` 
+        <div class="gallery__item">
+            <a class="gallery__link" 
+            href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join('');
+
+    return markup
+
+}
+
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+
