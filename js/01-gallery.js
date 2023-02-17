8c9b66e2c6ff14e8d1a26198caf3d3d6b34b1d05
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-17 07:55:33 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,12 +9,16 @@ const galleryContainer = document.querySelector('.gallery');
 
 const cardsMarkup = createImagesCardsMarkup(galleryItems);
 
+galleryContainer.addEventListener('click', onGalleryPictureClick);
 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 
 
 
+
+
+// function that creates markup
 function createImagesCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return ` 
@@ -37,4 +41,33 @@ function createImagesCardsMarkup(galleryItems) {
 }
 
 // Реалізація делегування на div.gallery і отримання url великого зображення.
+ 
+function onGalleryPictureClick(e) {
+    e.preventDefault()
+    
+    const isGalleryImageEl = e.target.nodeName
+
+    if (isGalleryImageEl !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+    `)
+
+    instance.show();
+
+    galleryContainer.addEventListener('keydown', (evt) => {
+    if (evt.code === 'Escape') {
+
+        console.log(evt);
+        instance.close();
+    }
+    });
+    
+}
+
+
+
+// створюемо лайтбокс 
 
