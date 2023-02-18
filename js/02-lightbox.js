2288c9c4c210129e99f0de3395e830159b178b8b
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-18 16:20:36 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createImagesCardsMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryPictureClick);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// function that creates markup
+function createImagesCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return ` 
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    }).join('');
+
+    return markup
+
+}
+
+
+
+ 
+function onGalleryPictureClick(e) {
+    e.preventDefault()
+    
+    const isGalleryImageEl = e.target.nodeName
+
+    if (isGalleryImageEl !== 'IMG') {
+        return;
+    };
+
+    const lightbox = new SimpleLightbox(`.gallery a`, {
+        loop: true,
+        captionsData: `alt`,
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        showCounter: false,
+    });
+
+    console.log(e.target.alt);
+
+}
